c0038fc977b686db3fcfcf1989dc7572bcfb0a50
Karina <karinakrupodior@gmail.com>
2021-02-04 00:04:24 +0200

    buttons added

--- calendar/create_event/index.js
+++ calendar/create_event/index.js
@@ -1,7 +1,7 @@
-
-
 function store() {
+
     let formData = new FormData(document.querySelector('form'))
+    console.log(formData,'formData')
     let object = {};
 
     let memberName = ''
@@ -22,33 +22,57 @@ function store() {
 function showTasks() {
     let name = document.querySelector('#choose_members').value
     let personInfo = JSON.parse(localStorage.getItem(name))
+    console.log(personInfo,'personInfo')
     personInfo.forEach(personData => {
         let td = document.getElementById(`${personData.time}_${personData.day}`)
+        let div = document.createElement('div')
+        td.prepend(div)
 
-        td.innerHTML = '<button  id = button_delete  class="cl-btn-7"></button>';
-        td.prepend(personData.event_name)
-
-
-        let time_box = document.getElementById('button_delete ')
-        time_box.remove()
-        time_box.forEach(block=> block.addEventListener('click',removeBlock))
-        // function  removeBlock(){
-        //     let block = this;
-        //     block.style.opacity = 1;
-        //     let blockId =setInterval(function (){
-        //         if(block.style.opacity > 0) block.style.opacity -= .1;
-        //         else {
-        //             clearInterval(blockId)
-        //             block.remote()
-        //         }
-        //     },60)
-        // }
+        div.innerHTML = '<button  id = "button_delete"  type="reset" class="cl-btn-7"></button>';
+        div.prepend(personData.event_name)
 
 
+        document.getElementById("button_delete").onclick = function (e) {
+
+
+            button_delete.innerHTML = '<div id="my_modal" class="modal"></div>'
+            let my_modal = document.querySelector('#my_modal')
+            my_modal.innerHTML = '<div class="modal_content" id="modal_content"></div>'
+            let modal_content = document.querySelector('#modal_content')
+            modal_content.innerHTML = '<p class="text_modal">Are you sure you want to delete event?</p>'
+
+            modal_content.insertAdjacentHTML("beforeend", '<button  id = "button_no"  class="button_no">NO</button>')
+
+
+            modal_content.insertAdjacentHTML("beforeend", '<button  id = "button_yes"   class="button_yes">YES</button>')
+
+
+            let modal = document.getElementById("my_modal");
+            let button_no = document.getElementById('button_no')
+            let button_yes = document.getElementById('button_yes')
+
+
+            button_delete.onclick = function () {
+                modal.style.display = "block";
+            }
+            button_no.onclick = function () {
+               modal.remove()
+            }
+            button_yes.onclick = function () {
+                div.remove()
+            }
+
+
+            window.onclick = function (event) {
+                if (event.target === modal) {
+                    modal.style.display = "none";
+                }
+            }
+
+
+        }
 
     })
-
-
 
 }
 
