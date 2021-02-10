f264eab46fa6730d642825ea785e97594d0bf994
Karina <karinakrupodior@gmail.com>
2021-02-11 00:10:13 +0200

    project ready

--- calendar/create_event/index.js
+++ calendar/create_event/index.js
@@ -1,24 +1,12 @@
-function validate() {
-    let event_text = document.getElementById("event_text").value;
-    console.log(event_text)
-
-    if (event_text === "") {
-        event_text.style.background = "red";
-        return false;
-    }
-    return true;
-}
-
 function store() {
 
     history.pushState(null, null, 'http:/calendar/')
     history.go(0)
 
     let formData = new FormData(document.querySelector('form'))
-    console.log(formData, 'formData')
     let object = {};
 
-    let memberName = ''
+    let memberName = '';
 
     formData.forEach((value, key) => {
 
@@ -33,75 +21,84 @@ function store() {
     localStorage.setItem(memberName, JSON.stringify(itemsArray))
 }
 
+function clearAll() {
+
+    let alles_td = document.getElementsByTagName("td");
+
+    for (let i = 0; i < alles_td.length; i++) {
+
+        if(alles_td[i].className !== 'time_box'){
+            alles_td[i].innerHTML = '&nbsp;';
+        }
+    }
+}
 
 function showTasks() {
     let name = document.querySelector('#choose_members').value
-    let personInfo = JSON.parse(localStorage.getItem(name))
-    console.log(personInfo, 'personInfo')
+
+    let personInfo = localStorage.getItem(name)
+    let parsedPersonInfo = JSON.parse(personInfo);
+
+    if (name === 'All members'){
+        const items = {...localStorage}
+
+        Object.keys(items).forEach(name => {
+            let info = JSON.parse(items[name])
+            memberInfo(info)
+        })
+
+    } else {
+        memberInfo(parsedPersonInfo)
+    }
+}
+
+
+
+function memberInfo(personInfo){
+    let name = document.querySelector('#choose_members').value
+
     if (personInfo && personInfo.length) {
         personInfo.forEach(personData => {
+
             let td = document.getElementById(`${personData.time}_${personData.day}`)
             let div = document.createElement('div')
             td.prepend(div)
 
-            div.innerHTML = '<button  id = "button_delete"  type="reset" class="cl-btn-7" data-toggle="modal" data-target="#myModal"> </button>';
+            div.innerHTML = '<button  id = "button_delete"  class="cl-btn-7"></button>';
             div.prepend(personData.event_text)
+            let result = (personData.items)
 
             let button_delete = document.getElementById("button_delete");
 
-            document.getElementById("button_delete").onclick = function () {
-
+            document.getElementById("button_delete").onclick = () => {
 
                 button_delete.innerHTML = '<div id="my_modal" class="modal"></div>'
                 let my_modal = document.querySelector('#my_modal')
                 my_modal.innerHTML = '<div class="modal_content" id="modal_content"></div>'
                 let modal_content = document.querySelector('#modal_content')
                 modal_content.innerHTML = `<p class="text_modal">Are you sure you want to delete  ${personData.event_text} event?</p>`
-
-                modal_content.insertAdjacentHTML("afterbegin", '<span class="close_modal_window">X</span>')
-
-                modal_content.insertAdjacentHTML("beforeend", '<button type="button"  id = "button_no"  class="button_no"  aria-label="close" >NO</button>')
-
-
+                modal_content.insertAdjacentHTML("beforeend", '<button type="button"  id = "button_no"  class="button_no">NO</button>')
                 modal_content.insertAdjacentHTML("beforeend", '<button  id = "button_yes"  class="button_yes">YES</button>')
-
-
                 let modal = document.getElementById("my_modal");
                 let button_no = document.getElementById('button_no')
                 let button_yes = document.getElementById('button_yes')
-                let span = document.getElementsByClassName("close_modal_window")[0];
 
-
-                button_delete.onclick = function () {
-
-                    modal.classList.add('modal_vis');
-
+                button_delete.onclick = () => {
+                    modal.style.display = "block";
                 }
-                span.onclick = function () {
-                    modal.style.display = "none";
+                button_no.onclick = () => {
+                    div.style.display = "none";
                 }
-
-                button_no.onclick = function () {
-                    modal.classList.remove('modal_vis');
-                    modal.style.display = "none";
-
-                }
-
-                button_yes.onclick = function () {
+                button_yes.onclick = () => {
                     localStorage.removeItem(name);
-                    div.remove()
+                    div.remove();
                 }
-
-
-
-                window.onclick = function (event) {
+                window.onclick = (event) => {
                     if (event.target === modal) {
                         modal.style.display = "none";
                     }
                 }
             }
-
         })
     }
 }
-
